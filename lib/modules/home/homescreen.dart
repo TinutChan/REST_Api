20f1292ab2aaf3726731d76f75b50fb1844013d3
TinutChan <tinutchan444@gmail.com>
2023-01-20 09:32:02 +0700

    test

--- lib/modules/home/homescreen.dart
+++ lib/modules/home/homescreen.dart
@@ -2,58 +2,136 @@ import 'package:app2022/modules/recommend_property/controller/recommend_property
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'controller/product_model_controller.dart';
-
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  final productsController = Get.put(ProductsController());
+  // final productsController = Get.put(ProductsController());
   final recommendProductsController = Get.put(RecommendPropertyController());
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Recommend Property',
-                style: Theme.of(context)
-                    .textTheme
-                    .headline5!
-                    .merge(const TextStyle(color: Colors.black)),
+    return Obx(
+      () => Scaffold(
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Recommend Property',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline5!
+                      .merge(const TextStyle(color: Colors.black)),
+                ),
               ),
-            ),
-            Center(
-              child: IconButton(
-                onPressed: recommendProductsController.getRecommendProperty,
-                icon: const Icon(Icons.refresh),
+              Center(
+                child: IconButton(
+                  onPressed: () {
+                    debugPrint('=============');
+                    recommendProductsController.getRecommendProperty();
+                  },
+                  icon: const Icon(Icons.refresh),
+                ),
               ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount:
-                    recommendProductsController.listRecommendproperties.length,
-                itemBuilder: ((context, index) {
-                  return Column(
-                    children: recommendProductsController
-                        .listRecommendproperties
-                        .map((element) {
-                      return Container(
-                        width: double.infinity,
-                        height: 200,
-                        color: Colors.red,
-                      );
-                    }).toList(),
-                  );
-                }),
+              if (recommendProductsController.isLoading.value)
+                const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(10.0),
+                  itemCount: recommendProductsController
+                      .listRecommendproperties.length,
+                  itemBuilder: ((context, index) {
+                    return Wrap(
+                      children: recommendProductsController
+                          .listRecommendproperties
+                          .map((element) {
+                        return Container(
+                          margin: const EdgeInsets.symmetric(vertical: 10.0),
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0),
+                              boxShadow: const [
+                                BoxShadow(
+                                    color: Colors.grey,
+                                    blurStyle: BlurStyle.solid,
+                                    blurRadius: 1.0,
+                                    offset: Offset(0.3, 0.3))
+                              ]),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Stack(
+                                children: [
+                                  Container(
+                                    width: double.infinity,
+                                    height: 198,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                      image: DecorationImage(
+                                        fit: BoxFit.cover,
+                                        image:
+                                            NetworkImage(element.profile![0]),
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    right: 0,
+                                    top: 0,
+                                    child: IconButton(
+                                        onPressed: () {},
+                                        icon:
+                                            const Icon(Icons.favorite_border)),
+                                  ),
+                                  Positioned(
+                                    bottom: 0,
+                                    left: 0,
+                                    right: 0,
+                                    child: Container(
+                                      width: double.infinity,
+                                      height: 27,
+                                      decoration: BoxDecoration(
+                                        borderRadius: const BorderRadius.only(
+                                            bottomRight: Radius.circular(10.0),
+                                            bottomLeft: Radius.circular(10.0)),
+                                        color: const Color(0xff213A8C)
+                                            .withOpacity(0.32),
+                                      ),
+                                      child: Row(
+                                        children: [
+                                          Text('${element.price}'),
+                                        ],
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Text('${element.name}'),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text('${element.type}'),
+                                  Text('${element.bed}')
+                                ],
+                              ),
+                              Text('${element.address}')
+                            ],
+                          ),
+                        );
+                      }).toList(),
+                    );
+                  }),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/modules/recommend_property/controller/recommend_property_controller.dart
+++ lib/modules/recommend_property/controller/recommend_property_controller.dart
@@ -7,25 +7,36 @@ import 'package:http/http.dart' as http;
 import '../models/recommend_property_model.dart';
 
 class RecommendPropertyController extends GetxController {
-  final listRecommendproperties = <RecommendProperty>[].obs;
-  final recommendProperty = RecommendProperty().obs;
+  var listRecommendproperties = <RecommendProperty>[].obs;
+  var recommendProperty = RecommendProperty().obs;
+  var isLoading = false.obs;
+
   Future getRecommendProperty() async {
-    String url =
-        'https://pre-rentkh.z1central.com/api/mobile/v2/recommend-properties?type=&address=&bed=&name_or_code=&max=&min=&page=';
-    await http.get(Uri.parse(url), headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    }).then((response) {
-      debugPrint('=====================$response');
-      var responseJson = json.decode(response.body)['data'];
-      debugPrint('===============Json=========$responseJson');
-      responseJson.map((response) {
-        recommendProperty.value = RecommendProperty.fromJson(response);
-        debugPrint('================Respond=======$recommendProperty');
-        listRecommendproperties.add(recommendProperty.value);
-        debugPrint(
-            '================List Recommend=======$listRecommendproperties');
-      }).toList();
-    });
+    isLoading.value = true;
+    debugPrint('========GetFuntion');
+    try {
+      String url =
+          'https://pre-rentkh.z1central.com/api/mobile/v2/recommend-properties?type=&address=&bed=&name_or_code=&max=&min=&page=';
+      await http.get(Uri.parse(url), headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }).then((response) {
+        debugPrint('=====================${response.body}');
+        var responseJson = json.decode(response.body)['data'];
+        // debugPrint('===============Json=========$responseJson');
+        responseJson.map((response) {
+          recommendProperty.value = RecommendProperty.fromJson(response);
+          // debugPrint('================Respond=======$recommendProperty');
+          listRecommendproperties.add(recommendProperty.value);
+          // debugPrint(
+          //     '================List Recommend=======$listRecommendproperties');
+        }).toList();
+      });
+    } catch (e) {
+      debugPrint('===============Catch $e');
+    } finally {
+      isLoading.value = false;
+      debugPrint('=============finally');
+    }
   }
 }
